90d7838b240cf8392f2778e023102ff8e8a38564
Jefriwahyu <memberjefry1@gmail.com>
2022-03-07 19:47:41 +0700

    Tugas 10 Javascript
    Tugas ini saya kumpulkan untuk NIOMIC.

---
+++ index.js
@@ -0,0 +1,45 @@
+function nomor() {
+  let n = 1;
+  for (var i = 0; i < 1; i++) {
+    console.log(i + n)
+  }
+  n++;
+  for (var i = 1; i < 2; i++) {
+    console.log(i + n)
+  }
+  n++;
+  for (var i = 3; i < 4; i++) {
+    console.log(i + n)
+  }
+  n++;
+  for (var i = 6; i < 7; i++) {
+    console.log(i + n)
+  }
+  n++;
+  for (var i = 10; i < 11; i++) {
+    console.log(i + n)
+  }
+  n++;
+  for (var i = 15; i < 16; i++) {
+    console.log(i + n)
+  }
+  n++;
+  for (var i = 21; i < 22; i++) {
+    console.log(i + n)
+  }
+  n++;
+  for (var i = 28; i < 29; i++) {
+    console.log(i + n)
+  }
+  n++;
+  for (var i = 36; i < 37; i++) {
+    console.log(i + n)
+  }
+  n++;
+  for (var i = 45; i < 46; i++) {
+    console.log(i + n)
+  }
+
+}
+
+nomor()
