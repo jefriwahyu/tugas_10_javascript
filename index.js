9ec5e0d199694c5cb65913da382eb5e4c545c2cc
jefriwahyu <92837751+jefriwahyu@users.noreply.github.com>
2022-08-13 08:11:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,20 @@
 function nomor() {
-  let n = 1;
-  for (var i = 0; i < 1; i++) {
-    console.log(i + n)
+
+  var angka = {
+     a1 : 1,
+     a2 : 3,
+     a3 : 6,
+     a4 : 10,
+     a5 : 15,
+     a6 : 21,
+     a7 : 28,
+     a8 : 36,
+     a9 : 45,
+     a10 : 55
   }
-  n++;
-  for (var i = 1; i < 2; i++) {
-    console.log(i + n)
-  }
-  n++;
-  for (var i = 3; i < 4; i++) {
-    console.log(i + n)
-  }
-  n++;
-  for (var i = 6; i < 7; i++) {
-    console.log(i + n)
-  }
-  n++;
-  for (var i = 10; i < 11; i++) {
-    console.log(i + n)
-  }
-  n++;
-  for (var i = 15; i < 16; i++) {
-    console.log(i + n)
-  }
-  n++;
-  for (var i = 21; i < 22; i++) {
-    console.log(i + n)
-  }
-  n++;
-  for (var i = 28; i < 29; i++) {
-    console.log(i + n)
-  }
-  n++;
-  for (var i = 36; i < 37; i++) {
-    console.log(i + n)
-  }
-  n++;
-  for (var i = 45; i < 46; i++) {
-    console.log(i + n)
+
+  for (var a in angka) {
+    console.log(angka[a]);
   }
 
 }
